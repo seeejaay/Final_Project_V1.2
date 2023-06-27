50a3753d4f32b828e25257c81a3e1f3c3e37c552
Carl Justine F. Malabanan <carljustinem984@gmail.com>
2023-06-28 02:36:05 +0800

    Project Final File

---
+++ script.js
@@ -0,0 +1,60 @@
+const hamburgerMenu = document.querySelector(".hamburger");
+const navGroup = document.querySelector(".nav-group");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navGroup.classList.toggle("active");
+  console.log("Hamburger is activated");
+});
+
+document.querySelectorAll(".nav-link").forEach((l) =>
+  l.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    navGroup.classList.remove("active");
+    console.log("Hamburger deactivated");
+  })
+);
+
+const openHome = () => {
+  let openH = window.open("index.html", "_self");
+  console.log("Opened Home: " + openH);
+};
+
+const openAbout = () => {
+  let openA = window.open("about.html", "_self");
+  console.log("Opened About: " + openA);
+};
+
+const openGallert = () => {
+  let openG = window.open("gallery.html", "_self");
+  console.log("Opened Gallery: " + openG);
+};
+
+const openProjects = () => {
+  let openP = window.open("projects.html", "_self");
+  console.log("Opened Projects: " + openP);
+};
+
+const openContact = () => {
+  let openC = window.open("contact.html", "_self");
+  console.log("Opened Contacts: " + openC);
+};
+
+const openLinked = () => {
+  let openL = window.open(
+    "https://www.linkedin.com/in/carl-justine-malabanan-2a868723a/",
+    "_blank"
+  );
+};
+
+const openTwitter = () => {
+  let openT = window.open("https://twitter.com/@managerd26", "_blank");
+};
+
+const openGithub = () => {
+  let openT = window.open("https://github.com/managerd", "_blank");
+};
+
+const openYoutube = () => {
+  let openY = window.open("https://www.youtube.com/@managerd4385", "_blank");
+};
